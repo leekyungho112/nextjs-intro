763e968b08ff39cd40acd4a265aefaae0c01ca36
kyungho <epsoe@naver.com>
2022-01-07 19:37:31 +0900

    styles jsx

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from './NavBar.module.css';
 
 const NavBar = () => {
   const router = useRouter();
@@ -8,13 +7,22 @@ const NavBar = () => {
   return (
     <nav>
       <Link href="/">
-        <a className={router.pathname === '/' ? styles.active : ''}>Home</a>
+        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
       <Link href="/about">
-        <a className={router.pathname === '/about' ? styles.active : ''}>
-          About
-        </a>
+        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 };
