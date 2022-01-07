9aab528120ae962c3012bb0dfe889e23010575e9
kyungho <epsoe@naver.com>
2022-01-07 15:04:31 +0900

    Routing

---
+++ components/NavBar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const NavBar = () => {
+  const router = useRouter();
+  console.log(router);
+  return (
+    <nav>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const { default: NavBar } = require('../components/NavBar');
+
+const About = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
